9331fcfaf09aa5ca300412dece64bd4dc1294a93
Krushnaraj004 <Krushnaraj11@gmail.com>
2021-05-31 19:35:17 +0530

    first commit

---
+++ Q1a.js
@@ -0,0 +1,17 @@
+function fun() {
+    var a = document.getElementById("main").style.background = "green";
+}
+
+function fun2() {
+    var a = document.getElementById("main").style.background = "red";
+}
+
+
+function fun3() {
+    var a = document.getElementById("main").style.background = "blue";
+}
+
+
+function fun4() {
+    var a = document.getElementById("main").style.background = "grey";
+}
